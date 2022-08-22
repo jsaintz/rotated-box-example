b53de1f3b3ecfdacd3a2bd020f53f8e61f293c96
Jonatas Santos <jonatas.santos@unicornio.academy>
2022-08-22 09:47:02 -0300

    created Rotated Box

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -8,13 +8,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late int _turns = 0;
+  late double _value = 0.0;
+
+  void _onChanged(double value) {
+    setState(() {
+      _value = value;
+      _turns = value.toInt();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Rotated Box'),
       ),
-      body: Container(),
+      body: Container(
+        padding: const EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: [
+              Slider(
+                value: _value,
+                onChanged: _onChanged,
+                min: 0.0,
+                max: 4.0,
+              ),
+              RotatedBox(
+                quarterTurns: _turns,
+                child: const Text('Hello World'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
